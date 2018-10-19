61da49c13e11266252e8d467c426e976b1a8f53d
Ryan Johnston <ryanpj@gmail.com>
2018-10-19 19:13:16 -0400

    Basic react structure

---
+++ react/src/components/account/AccountContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class AccountContainer extends React.Component {
+  render() {
+    return (
+      <div className="AccountContainer">
+        Account Container
+      </div>);
+  }
+}
