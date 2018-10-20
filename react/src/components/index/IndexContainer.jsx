5586a3f99bae1e7a099d51e1080d09024ec0c4b4
Tyler Phillips <tphi@umich.edu>
2018-10-20 02:16:04 -0400

    Added basic scaffolding for a home page

--- react/src/components/index/IndexContainer.jsx
+++ react/src/components/index/IndexContainer.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Header,Menu,Dropdown } from 'semantic-ui-react';
 
 export default class IndexContainer extends React.Component {
   render() {
+    const leftPanel = (
+      <Menu.Menu>
+         <Menu.Item header>Home</Menu.Item>
+         <Menu.Item>Q/A</Menu.Item>
+         <Menu.Item>Chat</Menu.Item>
+      </Menu.Menu>
+    );
+
+    const rightPanel = (
+      <Menu.Menu position="right">
+        <Menu.Item position="right">Mentor Sign Up</Menu.Item>
+        <Menu.Item position="right">Mentee Sign Up</Menu.Item>
+      </Menu.Menu>
+    );
+
+
     return (
       <div className="IndexContainer">
+        <Menu>
+          {leftPanel}
+          {rightPanel}
+        </Menu>
+
         <Grid columns="equal">
           <Grid.Row>
             <Grid.Column></Grid.Column>
@@ -12,6 +33,26 @@ export default class IndexContainer extends React.Component {
               <Header as="h1" textAlign='center' style={{ textAlign: 'center', marginTop: '50px' }}>
                 <Header.Content>GRSI Mentor Matcher</Header.Content>
               </Header>
+              <p> Community-based sponsorship programs allow individuals to
+                directly engage in refugee resettlement efforts.
+                Sponsors commit to providing financial, emotional and
+                resettlement support to help newly-arrived refugees integrate
+                into life in a new country.
+                The Canadian government has resettled more than 300,000*
+                refugees through its Private Sponsorship of Refugees program
+                since it was introduced in 1979. In addition, hundreds of
+                communities across Canada have benefited by coming together to
+                welcome privately sponsored refugees.</p>
+
+              <Header as="h4">Select a country below to learn more about the situation in your current country</Header>
+            <Dropdown text='Select Country'>
+                <Dropdown.Menu>
+                <Dropdown.Item text='Canada' />
+                <Dropdown.Item text='Great Britian'/>
+                <Dropdown.Item text='Germany'/>
+                </Dropdown.Menu>
+            </Dropdown>
+
             </Grid.Column>
             <Grid.Column></Grid.Column>
           </Grid.Row>
