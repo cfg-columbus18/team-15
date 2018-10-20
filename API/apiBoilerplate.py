644854feb4e7f96a8e28f6b945e76887db7772a5
Aaron Lopez <dazdndcunfusd@gmail.com>
2018-10-20 02:12:25 -0400

    switched to mysql database querying.
    switched to mysql database querying.

--- API/apiBoilerplate.py
+++ API/apiBoilerplate.py
@@ -14,71 +14,74 @@ from flask.cli import with_appcontext
 
 ##connects to database
 
-def get_mysqlDB():
+def get_db():
 	db = mysql.connector.connect(host = "localhost",
 					  port = "3306",
 					  user= "root",
 					  passwd = "root",
-					  db = "world")
-	curA = db.cursor()
-	query = ("SELECT * FROM city")
-	curA.execute(query)
-	for(name) in curA:
-		print name
+					  db = "team15")
+	return db
+	
 
-get_mysqlDB()
+def user_profile(mentor, db):##TODO:change users to correct table.
+	cursor = db.cursor(buffered = True)
+	query = ("SELECT * FROM mentor WHERE name = %(name)s")
+	mentor_data = {
+		'name' : mentor[name]
+		}
+	cursor.execute(query,mentor_data )
+	return query
 
-def get_db():
-	GSRI = client.GSRI
-##
-	return GSRI
-
-def close_db():
-	db = g.pop('db', None)
-	if db is not None:
-		db.close()
-
-
-def user_profile(mentorusername, db):##TODO:change users to correct table.
-	mentors = db["Mentors"]
-	user = db.mentors.find_one_or_404({"_id": mentorusername})
-	return user
 
 ##queries for mentor table when needed.
 def getMentorTable(db):
-	mentorTable = db["Mentors"]
+	cursor = db.cursor(buffered = True)
+	mentorTable = ("SELECT * FROM mentor")
+	cursor.execute(mentorTable)
+
 	return mentorTable
 
 def createMentor(mentor, db):
-	mentors = db["Mentors"]
-	mentorInsert = {
-		'Name' : mentor[name],
-		'Experience' : mentor[experience],
-		'timezone' : mentor[TZ],
-		'phone' : mentor[contact],
-		'email' : mentor[email],
-		'location' : mentor[location],
-		'expertise': mentor[expertise],
+	cursor = db.cursor(buffered = True)
+	mentorInsert =  ("INSERT INTO mentor "
+				  "(idmentee, name, country, timezone, experience, phone, language, preferred_platform, sponsorship_stage, expertise, email, bio)"
+				 "VALUES (%(name)s, %(country)s, %(timezone)s, %(experience)s, %(phone)s, %(language)s, %(preferred_platform)s, %(sponsorship_stage)s, %(expertise)s, %(email)s, %(bio)s)")
+	data_mentor = {
+		'name' : mentor[name],
+		'country' : mentor[country],
+		'timezone' : mentor[timezone],
+		'experience' : mentor[experience],
+		'phone' : mentor[phone],
 		'language' : mentor[language],
-		'platform' : mentor[platform],
-		'sponsorstage' : mentor[sponsorStage],
-		'bio' : mentor[Bio]
+		'preferred_platform' : mentor[preferred_platform],
+		'sponsorship_stage' : mentor[sponsorship_stage],
+		'expertise' : mentor[expertise],
+		'email' : mentor[email],
+		'bio' : mentor[bio]
 		}	
-	mentors.insert_one(mentorInsert)
+	cursor.execute(mentorInsert, data_mentor)
+	db.commit()
+	cursor.close()
 
 def createMentee(mentee, db):
-	mentees = db["Mentees"]
-	menteeInsert = {
-		'name' : mentee[Name],
+	cursor = db.cursor(buffered = True)
+	menteeInsert =  ("INSERT INTO mentee "
+				  "(idmentee, name, country, timezone, experience, phone, language, preferred_platform, sponsorship_stage, expertise, email, bio)"
+				 "VALUES (%(name)s, %(country)s, %(timezone)s, %(experience)s, %(phone)s, %(language)s, %(preferred_platform)s, %(sponsorship_stage)s, %(expertise)s, %(email)s, %(bio)s)")
+	data_mentee = {
+		'name' : mentee[name],
+		'country' : mentee[country],
+		'timezone' : mentee[timezone],
 		'experience' : mentee[experience],
-		'timezone' : mentee[TZ],
-		'phone' : mentee[contact],
-		'email' : mentee[email],
-		'location' : mentee[location],
-		'expertise': mentee[expertise],
+		'phone' : mentee[phone],
 		'language' : mentee[language],
-		'platform' : mentee[platform],
-		'sponsorstage' : mentee[sponsorStage],
-		'bio' : mentee[Bio]
+		'preferred_platform' : mentee[preferred_platform],
+		'sponsorship_stage' : mentee[sponsorship_stage],
+		'expertise' : mentee[expertise],
+		'email' : mentee[email],
+		'bio' : mentee[bio]
 		}
-	mentee.insert_one(menteeInsert)
+
+	cursor.execute(menteeInsert, data_mentee)
+	db.commit()
+	cursor.close()
