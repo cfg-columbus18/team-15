b9a6f4217462d8a071c4ec481fa9b0f00b135f72
Tyler Phillips <tphi@umich.edu>
2018-10-19 23:53:23 -0400

    Updated Search and Profile

--- react/src/components/Router.jsx
+++ react/src/components/Router.jsx
@@ -4,11 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import IndexContainer from './index/IndexContainer';
 import ProfileContainer from './profile/ProfileContainer';
 import SignUpContainer from './signup/SignUpContainer';
+import SearchContainer from './search/SearchContainer';
+
 
 const Router = () => (
   <Switch>
     <Route exact path="/" component={IndexContainer} />
     <Route exact path="/profile" component={ProfileContainer} />
+    <Route exact path="/search" component={SearchContainer} />
     <Route exact path="/signup/:role" component={SignUpContainer} />
     {/* <Route component={404} /> */}
   </Switch>
