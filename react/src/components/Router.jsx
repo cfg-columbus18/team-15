b6730646b69b78c9febd6ab296a09564ccfaa43e
Tyler Phillips <tphi@umich.edu>
2018-10-19 21:34:51 -0400

    Added search feature and began work on profile screen

--- react/src/components/Router.jsx
+++ react/src/components/Router.jsx
@@ -3,11 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import ProfileContainer from './profile/ProfileContainer';
 import IndexContainer from './index/IndexContainer';
+import SearchContainer from './search/SearchContainer'
 
 const Router = () => (
   <Switch>
     <Route exact path="/" component={IndexContainer} />
     <Route exact path="/profile" component={ProfileContainer} />
+    <Route exact path="/search" component={SearchContainer} />
+
     {/* TODO <Route component={404}/> */}
   </Switch>
 );
--- react/src/components/profile/ProfileContainer.jsx
+++ react/src/components/profile/ProfileContainer.jsx
@@ -5,36 +5,51 @@ export default class ProfileContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      profile: {'a':'b'},
+      profiles: {
+        a: 'b',
+        c: 'd',
+      },
     };
   }
 
-  componentDidMount() {
-    this.loadData();
-  }
-
-  loadData() {
-    // Call REST API to get a form data from database
-    fetch(this.props.url, { credentials: 'same-origin' })
-      .then((response) => {
-        if (!response.ok) throw Error(response.statusText);
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({
-          profile: data.profile,
-        });
-      })
-      .catch(error => console.log(error)); // eslint-disable-line no-console
-  }
+  // componentDidMount() {
+  //   this.loadData();
+  // }
+  //
+  // loadData() {
+  //   // Call REST API to get form data from database
+  //   fetch(this.props.url, { credentials: 'same-origin' })
+  //     .then((response) => {
+  //       if (!response.ok) throw Error(response.statusText);
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       this.setState({
+  //         profiles: data.profile,
+  //       });
+  //     })
+  //     .catch(error => console.log(error)); // eslint-disable-line no-console
+  // }
 
   render() {
+    const { profiles } = this.state;
 
+
+    const output = Object.keys(profiles).map(key => (
+      <div>
+        <p>
+          {key}
+          -
+          {profiles[key]}
+        </p>
+      </div>
+    ));
 
 
     return (
       <div className="ProfileContainer">
         Profile Container
+        {output}
       </div>);
   }
 }
